085965034dc68bdda6ab6839df983269d96b32ab
Daniel Fernández Marqués <daniFMdev@gmail.com>
2020-07-11 11:04:30 +0200

    Added password hide feature. Everything seems to be working fine. Added icon pack

---
+++ DnD_MapTool/Assets/Scripts/UI/InputPassword.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputPassword : MonoBehaviour
+{
+    private InputField field;
+
+    void Start()
+    {
+        field = GetComponent<InputField>();
+    }
+
+    public void ActivateView(bool active)
+    {
+        if(active)
+        {
+            field.contentType = InputField.ContentType.Standard;
+        }    
+        else
+        {
+            field.contentType = InputField.ContentType.Password;
+        }
+
+        field.ForceLabelUpdate();
+    }
+}
