67f07691fcacc8ff25c6363edcad9923209bb935
Daniel Fernández Marqués <daniFMdev@gmail.com>
2020-07-18 17:49:13 +0200

    Fixed Quit feature

---
+++ DnD_MapTool/Assets/Scripts/UI/QuitGameButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGameButton : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GetComponent<Button>().onClick.AddListener(QuitGame);
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<Button>().onClick.RemoveListener(QuitGame);
+    }
+
+    public void QuitGame()
+    {
+        GameManager.instance.QuitGame();
+    }
+}
