371333ddc8054c9cfa6b4729ededb13a8dc77dff
Daniel Fernández Marqués <daniFMdev@gmail.com>
2020-07-27 20:21:11 +0200

    Screenshot started working with new Explorer class. A new method is needed, as this one just works for one camera

---
+++ DnD_MapTool/Assets/ScreenShotCamera/ScreenShotController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShotController: MonoBehaviour
+{
+    public int resWidth = 1920;
+    public int resHeight = 1080;
+
+    private const string defaultName = "map";
+    private const string extension = "png";
+
+    //public KeyCode key;
+
+    //private bool takeShot = false;
+    private string path;
+
+    //void LateUpdate()
+    //{
+    //    takeShot |= Input.GetKeyDown(key);
+
+    //    if(takeShot && !string.IsNullOrEmpty(path))
+    //    {
+    //        Camera camera = GetComponent<Camera>();
+    //        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+    //        camera.targetTexture = rt;
+    //        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+    //        camera.Render();
+    //        RenderTexture.active = rt;
+    //        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+    //        camera.targetTexture = null;
+    //        RenderTexture.active = null; // JC: added to avoid errors
+    //        Destroy(rt);
+    //        byte[] bytes = screenShot.EncodeToPNG();
+    //        string filename = NameScreenShot(resWidth, resHeight);
+    //        System.IO.File.WriteAllBytes(filename, bytes);
+    //        Debug.Log("Took screenshot to: " + filename);
+    //        takeShot = false;
+    //    }
+    //}
+
+    public void TakeScreenshot()
+    {
+        Explorer.PathLoaded += OnPathLoaded;
+        Explorer.instance.GetPath(defaultName, extension);
+    }
+
+    private void OnPathLoaded(string path)
+    {
+        Explorer.PathLoaded -= OnPathLoaded;
+        StartCoroutine(TakeScreenshotRoutine(path));
+    }
+
+    private IEnumerator TakeScreenshotRoutine(string path)
+    {
+        // Deactivate UI
+        //...
+
+        yield return new WaitForEndOfFrame();
+
+        Camera camera = GetComponent<Camera>();
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+        camera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        camera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        camera.targetTexture = null;
+        RenderTexture.active = null; // JC: added to avoid errors
+        Destroy(rt);
+
+        byte[] bytes = screenShot.EncodeToPNG();
+        System.IO.File.WriteAllBytes(path, bytes);
+        Debug.Log("Took screenshot to: " + path);
+
+        yield return null;
+
+        // Activate UI
+        //...
+    }
+
+    //public static string NameScreenShot(int width, int height)
+    //{
+    //    string folder_path = Application.dataPath + "/screenshots";
+
+    //    if(!System.IO.Directory.Exists(folder_path))
+    //        System.IO.Directory.CreateDirectory(folder_path);
+
+    //    return string.Format("{0}/screen_{1}x{2}_{3}.png",
+    //                         folder_path,
+    //                         width, height,
+    //                         System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    //}
+}
---
+++ DnD_MapTool/Assets/Scripts/Utilities/Explorer.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Text;
+using SFB;
+
+public class Explorer : MonoBehaviour
+{
+    public GameObject browserPanel;
+
+    private string name;
+    private string extension;
+
+    public static Action<string> PathLoaded;
+    public static Action<string> DataLoaded;
+
+    public static Explorer instance;
+
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else if(instance != this)
+            Destroy(gameObject);
+    }
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+
+    [DllImport("__Internal")]
+    private static extern void DownloadFile(string gameObjectName, string methodName, string filename, byte[] byteArray, int byteArraySize);
+
+    [DllImport("__Internal")]
+    private static extern void UploadFile(string gameObjectName, string methodName, string filter, bool multiple);
+
+    // Broser plugin should be called in OnPointerDown.
+    public void Save(string data, string extension)
+    {
+        browserPanel.SetActive(true);
+
+        byte[] bytes = Encoding.UTF8.GetBytes(data);
+        DownloadFile(gameObject.name, "OnFileDownload", extension, bytes, bytes.Length);
+    }
+
+    // Called from browser
+    public void OnFileDownload()
+    {
+        Debug.Log("File Successfully Downloaded");
+    }
+
+    public void LoadData(string extension)
+    {
+        browserPanel.SetActive(true);
+
+        UploadFile(gameObject.name, "OnFileUpload", "." + extension, false);
+    }
+
+    // Called from browser
+    public void OnFileUpload(string url)
+    {
+        StartCoroutine(OutputRoutine(url));
+    }
+
+    private IEnumerator OutputRoutine(string url)
+    {
+        var loader = new WWW(url);
+        yield return loader;
+        DataLoaded?.Invoke(loader.text);
+    }
+
+#else
+
+    private Thread explorerThreadSave;
+    private Thread explorerThreadLoad;
+
+    /// <summary>
+    /// Result is provided in PathLoaded event. You should subscribe before calling this.
+    /// </summary>
+    public void GetPath(string name, string extension)
+    {
+        this.name = name;
+        this.extension = extension;
+
+        #if UNITY_EDITOR
+            GetExplorerPathAsync();
+        #else
+            //new Thread(GetExplorerPathAsync).Start();
+            //explorerThreadSave.Start();
+            
+            explorerThreadSave = null;
+            explorerThreadSave = new Thread(GetExplorerPathAsync);
+            explorerThreadSave.Start();
+        #endif
+    }
+
+    private void GetExplorerPathAsync()
+    {
+        string path;
+        path = StandaloneFileBrowser.SaveFilePanel(
+            "Save Map",
+            JSONSaver.defaultPath + "/" + JSONSaver.defaultFolder,
+            name,
+            extension);
+
+        if(path != string.Empty)
+        {
+            int n = path.LastIndexOf('\\');
+            //name = path.Substring(n + 1, path.LastIndexOf('.') - n - 1);
+            string folder = path.Substring(0, n);
+
+            if(!System.IO.Directory.Exists(folder))
+                System.IO.Directory.CreateDirectory(folder);
+        }
+        else
+        {
+            Debug.Log("Player cancelled explorer");
+        }
+
+        PathLoaded?.Invoke(path);
+
+        #if UNITY_EDITOR
+
+        #else
+            explorerThreadSave.Abort();
+        #endif
+    }
+
+#endif
+}
