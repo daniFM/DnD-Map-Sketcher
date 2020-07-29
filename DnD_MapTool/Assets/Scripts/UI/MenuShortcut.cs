9b423f6b415581a496e3a4bf33d3c7f4fef6c975
Daniel Fernández Marqués <daniFMdev@gmail.com>
2020-07-29 18:44:56 +0200

    Added menu shortcuts. Working with Controls menu

---
+++ DnD_MapTool/Assets/Scripts/UI/MenuShortcut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuShortcut : MonoBehaviour
+{
+    public KeyCode key;
+    public GameObject menu;
+    public GameObject parentMenu;
+
+    //public bool active = false; 
+
+    void Update()
+    {
+        if(Input.GetKeyDown(key) && !parentMenu.activeSelf)
+        {
+            ToggleSubMenu();
+        }
+    }
+
+    private void ToggleSubMenu()
+    {
+        //active = !active;
+        menu.SetActive(!menu.activeSelf);
+    }
+}
