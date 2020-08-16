d88fff9d58e1006312428ad3815e97b719d8aa04
Daniel Fernández Marqués <daniFMdev@gmail.com>
2020-08-16 10:32:03 +0200

    Implemented DraggablePanel in Token menu

---
+++ DnD_MapTool/Assets/Scripts/UI/DraggablePanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggablePanel : MonoBehaviour, IDragHandler
+{
+    public float sensitivity = 1;
+
+    //void Start()
+    //{
+        
+    //}
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.Translate(Input.GetAxis("Mouse X") * sensitivity, Input.GetAxis("Mouse Y") * sensitivity, 0);
+    }
+}
