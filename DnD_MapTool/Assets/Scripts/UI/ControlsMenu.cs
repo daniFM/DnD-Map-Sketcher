319d238259f80d936de88eb67500757e3bc3ccef
John Smolinski <jsmo81@iastate.edu>
2020-08-30 16:00:34 -0500

    wip: tooltip popup added that waits for user input when selecting a control field

---
+++ DnD_MapTool/Assets/Scripts/UI/ControlsMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] toolButtons;
+
+    [SerializeField]
+    private GameObject configureButtonTooltip;
+    
+    private string currentButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toolButtons = GetComponentsInChildren<Button>();
+        foreach (Button button in toolButtons)
+        {
+            button.onClick.AddListener(delegate () 
+                                            {
+                                                configureButtonTooltip.SetActive(true);
+                                                configureButtonTooltip.GetComponentInChildren<Text>().text = button.name + ": ";
+                                                currentButton = button.name;
+                                            }
+                                        );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string inputString;
+        if (configureButtonTooltip.activeInHierarchy)
+        {
+            inputString = Input.inputString;
+            if(inputString != "")
+            {
+                configureButtonTooltip.SetActive(false);
+            }
+        }
+    }
+}
