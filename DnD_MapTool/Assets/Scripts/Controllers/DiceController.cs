34a353eaaea447fd010933e96a7e6aaef1d5759c
Daniel Fernández Marqués <daniFMdev@gmail.com>
2020-08-31 16:34:02 +0200

    wip: basic dice rolling with DiceController

---
+++ DnD_MapTool/Assets/Scripts/Controllers/DiceController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum dice { D4, D6, D8, D10, D20, D100 }
+
+public class DiceController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] dicePrefabs;
+
+    private Dice[,] dicePool;
+
+    void Start()
+    {
+        dicePool = new Dice[5, 5];
+
+        for(int i = 0; i < dicePrefabs.Length; ++i)
+        {
+            for(int j = 0; j < dicePool.GetLength(1); ++j)
+            {
+                dicePool[i, j] = Instantiate(dicePrefabs[i], null).GetComponent<Dice>();
+                dicePool[i, j].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    #region DEBUG
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            dicePool[0, 0].gameObject.SetActive(true);
+            Dice.Rolled += OnRollResult;
+            dicePool[0, 0].Roll();
+        }
+    }
+
+    private void OnRollResult(int result)
+    {
+        Dice.Rolled -= OnRollResult;
+        Debug.Log("Dice result: " + result);
+    }
+
+    #endregion
+}
--- DnD_MapTool/Assets/Scripts/Game/Dice.cs
+++ DnD_MapTool/Assets/Scripts/Game/Dice.cs
@@ -1,34 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
-    public RangeFloat spinForce;
+    [SerializeField] private RangeFloat spinForce;
 
+    private bool instantiated;
     private Rigidbody rb;
+    private Transform [] faces;
+
+    public static Action<int> Rolled;
 
     #region DEBUG
 
-    void Start()
+    public void Instantiate()
     {
         rb = GetComponent<Rigidbody>();
+
+        Transform[] transforms = transform.GetComponentsInChildren<Transform>();
+        faces = new Transform[transforms.Length - 1];
+        Array.Copy(transforms, 1, faces, 0, transforms.Length - 1);
+        //faces = transform.GetComponentsInChildren<Transform>();
+
         //Roll();
+        instantiated = true;
     }
 
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            Roll();
-        }
-    }
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.R))
+    //    {
+    //        Roll();
+    //    }
+    //}
 
     #endregion
 
     public void Roll()
     {
+        if(!instantiated)
+        {
+            Instantiate();
+        }
+        StartCoroutine(RollRoutine());
+    }
+
+    private IEnumerator RollRoutine()
+    {
         transform.Translate(0, 10, 0, Space.World);
         rb.AddTorque(spinForce.GetRandom(), spinForce.GetRandom(), spinForce.GetRandom(), ForceMode.Impulse);
+
+        yield return new WaitUntil(() => rb.IsSleeping());
+
+        float height = -Mathf.Infinity;
+        int face = 0;
+        for(int i = 0; i < faces.Length; ++i)
+        {
+            float fh = faces[i].position.y;
+            if(fh > height)
+            {
+                height = fh;
+                face = i;
+            }
+        }
+
+        Rolled?.Invoke(face + 1);
     }
 }
