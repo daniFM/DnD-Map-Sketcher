b6fe1f6a4263472f1e3e1a6659e1c928faf66f57
Daniel Fernández <danielfernandez@axesinmotion.com>
2022-12-14 19:39:18 +0100

    wip: change license

--- DnD_MapTool/Assets/Scripts/Utilities/Editor/SceneAutoLoader.cs
+++ DnD_MapTool/Assets/Scripts/Utilities/Editor/SceneAutoLoader.cs
@@ -1,4 +1,5 @@
 // Copyright (c) 2022 Daniel Fernández Marqués
+// Licensed under the GNU General Public License (GPL) version 3. See the LICENSE file for more details.
 
 //using UnityEngine;
 //using UnityEditor;
