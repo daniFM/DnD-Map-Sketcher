a764434b6c96083cee180923209947e267a03691
Daniel Fernandez <daniFMdev@gmail.com>
2024-11-27 17:03:34 +0100

    revert changes in chat

--- DnD_MapTool/Assets/Scripts/UI/Chat.cs
+++ DnD_MapTool/Assets/Scripts/UI/Chat.cs
@@ -10,7 +10,7 @@ using Photon.Pun;
 
 public class Chat : MonoBehaviourPun
 {
-    [SerializeField] private GameObject chatMessageTextPrefab;
+    [SerializeField] private Text chatMessageTextPrefab;
     [SerializeField] private Transform chatMessagesContainer;
     [SerializeField] private Dropdown diceNumber;
     [SerializeField] private Dropdown diceType;
@@ -53,7 +53,7 @@ public class Chat : MonoBehaviourPun
 
         if(!string.IsNullOrEmpty(message))
         {
-            Text messageText = Instantiate(chatMessageTextPrefab, chatMessagesContainer).GetComponentInChildren<Text>();
+            Text messageText = Instantiate(chatMessageTextPrefab, chatMessagesContainer).GetComponent<Text>();
 
             //messageText.text = "<color=" + GameController.instance.GetPlayerColor() + ">" + GameManager.instance.playerName + "</color>: " + message;
 
